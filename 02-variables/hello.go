741a07def7b50e4a5b81b9bcc25bf10aebd537ae
Patrick Almeida <patricksalmeida@gmail.com>
2023-07-07 15:45:21 -0300

    module: 02 - starting with variables

---
+++ 02-variables/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Patrick"
+	var age int
+	var version float32 = 1.1
+	fmt.Println("Olá sr.", name, "sua idade é", age)
+	fmt.Println("Este programa está na versão", version)
+}
