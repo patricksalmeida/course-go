612912659390913776b26fc8efe01053ef02c8c3
Patrick Almeida <patricksalmeida@gmail.com>
2023-07-09 20:42:11 -0300

    module: 13 - add insert produtos logic to persiste them

--- 13-webapp/controllers/produtos.go
+++ 13-webapp/controllers/produtos.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com.br/patricksalmeida/course-go/13-webapp/models"
 )
@@ -17,4 +19,29 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func New(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+
+		if err != nil {
+			log.Println("Erro na conversão do preço: ", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+
+		if err != nil {
+			log.Println("Erro na conversão do preço: ", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertido, quantidadeConvertida)
+	}
+
+	http.Redirect(w, r, "/", 301)
 }
