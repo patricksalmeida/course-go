2852b9792d3de77a62f758a094dbce1499a00821
Patrick Almeida <patricksalmeida@gmail.com>
2023-07-08 22:22:50 -0300

    module: 09 - creating methods with pointers

--- 09-pointers/main.go
+++ 09-pointers/main.go
@@ -10,19 +10,24 @@ type ContaCorrente struct {
 }
 
 func main() {
-	contaDoPatrick := ContaCorrente{
-		titular:       "Patrick Almeida",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         125.5,
+	contaDoPatrick := ContaCorrente{}
+	contaDoPatrick.titular = "Patrick Almeida"
+	contaDoPatrick.numeroAgencia = 589
+	contaDoPatrick.numeroConta = 123456
+	contaDoPatrick.saldo = 500
+
+	fmt.Println(contaDoPatrick.saldo)
+	fmt.Println(contaDoPatrick.Sacar(-100))
+	fmt.Println(contaDoPatrick.saldo)
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
 	}
-
-	var contaDaSabrina *ContaCorrente
-	contaDaSabrina = new(ContaCorrente)
-	contaDaSabrina.titular = "Sabrina Almeida"
-	contaDaSabrina.numeroAgencia = 589
-	contaDaSabrina.numeroConta = 123457
-	contaDaSabrina.saldo = 130.5
-
-	fmt.Println(contaDoPatrick, contaDaSabrina)
 }
