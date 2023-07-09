a9d4126b6e1feab581990c97f3f40d5637fcdfd7
Patrick Almeida <patricksalmeida@gmail.com>
2023-07-09 19:25:00 -0300

    module: 13 - add postgres integration

--- 13-webapp/main.go
+++ 13-webapp/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=example123 host=localhost sslmode=disable"
+
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -20,12 +36,39 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{"Camiseta", "Azul, bem bonita", 39., 5},
-		{"Tenis", "Confortável", 89., 3},
-		{"Fone", "Muito bom", 59., 2},
-		{"Produto novo", "Muito legal", 1.99, 10},
+	db := conectaComBancoDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Id = id
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
 	}
 
 	templates.ExecuteTemplate(w, "Index", produtos)
+
+	defer db.Close()
 }
