2bb7d4bf89d81163a9b6cdd5b2e2fe7d0b239480
Patrick Almeida <patricksalmeida@gmail.com>
2023-07-09 21:30:53 -0300

    module: 13 - add update product feature

--- 13-webapp/models/produto.go
+++ 13-webapp/models/produto.go
@@ -13,7 +13,7 @@ type Produto struct {
 func BuscaTodosOsProdutos() []Produto {
 	db := db.ConectaComBancoDeDados()
 
-	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos order by id asc")
 
 	if err != nil {
 		panic(err.Error())
@@ -72,6 +72,54 @@ func DeletarProduto(id string) {
 	}
 
 	deletarOProduto.Exec(id)
+
+	defer db.Close()
+}
+
+func EditarProduto(id string) Produto {
+	db := db.ConectaComBancoDeDados()
+
+	produtoDoBanco, err := db.Query("select * from produtos where id = $1", id)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produtoParaAtualizar := Produto{}
+
+	for produtoDoBanco.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err := produtoDoBanco.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtoParaAtualizar.Id = id
+		produtoParaAtualizar.Nome = nome
+		produtoParaAtualizar.Descricao = descricao
+		produtoParaAtualizar.Preco = preco
+		produtoParaAtualizar.Quantidade = quantidade
+	}
+
+	defer db.Close()
+
+	return produtoParaAtualizar
+}
+
+func AtualizarProduto(id, quantidade int, nome, descricao string, preco float64) {
+	db := db.ConectaComBancoDeDados()
+
+	atualizarNoBanco, err := db.Prepare("update produtos set nome=$1, descricao=$2, preco=$3, quantidade=$4 where id=$5")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	atualizarNoBanco.Exec(nome, descricao, preco, quantidade, id)
 
 	defer db.Close()
 }
