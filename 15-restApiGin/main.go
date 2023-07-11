d1d0d6d70a799a38d88c2b006bb520f601d0c164
Patrick Almeida <patricksalmeida@gmail.com>
2023-07-11 09:47:59 -0300

    module: 15 - add database postgres connection

--- 15-restApiGin/main.go
+++ 15-restApiGin/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"github.com.br/patricksalmeida/course-go/15-restApiGin/database"
 	"github.com.br/patricksalmeida/course-go/15-restApiGin/models"
 	"github.com.br/patricksalmeida/course-go/15-restApiGin/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
-		{"Patrick Almeida", "12312312300", "991234567"},
-		{"Sabrina Almeida", "12312312400", "991234568"},
+		{Nome: "Patrick Almeida", CPF: "12312312300", RG: "991234567"},
+		{Nome: "Sabrina Almeida", CPF: "12312312400", RG: "991234568"},
 	}
 
 	routes.HandleRequests()
