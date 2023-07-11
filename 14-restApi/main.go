260aa762018a6f86560211f1b664719eccb85c0f
Patrick Almeida <patricksalmeida@gmail.com>
2023-07-10 21:07:51 -0300

    module: 14 - add database connection with gorm

--- 14-restApi/main.go
+++ 14-restApi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com.br/patricksalmeida/course-go/14-restApi/database"
 	"github.com.br/patricksalmeida/course-go/14-restApi/models"
 	"github.com.br/patricksalmeida/course-go/14-restApi/routes"
 )
@@ -12,6 +13,8 @@ func main() {
 		{ID: 1, Nome: "Nome 1", Historia: "História 1"},
 		{ID: 2, Nome: "Nome 2", Historia: "História 2"},
 	}
+
+	database.ConectaComBancoDeDados()
 
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
