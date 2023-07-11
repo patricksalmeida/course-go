5860725ecc0b2e669eaf1797bb2290dafdecd16d
Patrick Almeida <patricksalmeida@gmail.com>
2023-07-11 20:35:23 -0300

    feat: search students into db

--- 15-restApiGin/main.go
+++ 15-restApiGin/main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"github.com.br/patricksalmeida/course-go/15-restApiGin/database"
-	"github.com.br/patricksalmeida/course-go/15-restApiGin/models"
 	"github.com.br/patricksalmeida/course-go/15-restApiGin/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Patrick Almeida", CPF: "12312312300", RG: "991234567"},
-		{Nome: "Sabrina Almeida", CPF: "12312312400", RG: "991234568"},
-	}
 
 	routes.HandleRequests()
 }
